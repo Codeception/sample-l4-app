a80a838bb6c69ae76b97c99fd79c2a61c5365fc5
Davert <davert.php@resend.cc>
2014-02-23 00:58:45 +0200

    moved AspectMock to its own branch

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -1,16 +1,1 @@
 <?php
-// include_once '/home/davert/Codeception/src/Codeception/Platform/Extension.php';
-// include_once '/home/davert/Notifier/src/Codeception/Extension/UbuntuNotifier.php';
-// include_once '/home/davert/Notifier/src/Codeception/Extension/EmailNotifier.php';
-include __DIR__.'/../vendor/autoload.php';
-
-// $kernel = \AspectMock\Kernel::getInstance();
-// $kernel->init([
-//     'debug' => true,
-//     'cacheDir' => '/tmp/l4-sample',
-//     'includePaths' => [__DIR__.'/../vendor/laravel', __DIR__.'/../app']
-// ]);
-
-$app = require_once __DIR__.'/../bootstrap/start.php';
-
-\Codeception\Util\Autoload::registerSuffix('Page', __DIR__.DIRECTORY_SEPARATOR.'_pages');
