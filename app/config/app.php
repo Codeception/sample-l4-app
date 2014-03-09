dc91f9f499408644efedd84cd5f2441e6318fcb1
Davert <davert.php@resend.cc>
2014-03-09 23:50:21 +0200

    removed generator

--- app/config/app.php
+++ app/config/app.php
@@ -112,8 +112,7 @@ return array(
 		'Illuminate\Translation\TranslationServiceProvider',
 		'Illuminate\Validation\ValidationServiceProvider',
 		'Illuminate\View\ViewServiceProvider',
-		'Illuminate\Workbench\WorkbenchServiceProvider',
-		'Way\Generators\GeneratorsServiceProvider'
+		'Illuminate\Workbench\WorkbenchServiceProvider'
 
 	),
 
