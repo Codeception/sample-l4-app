eb2d1770f166b0689614817d12d129c569be2392
Jan-Henk Gerritsen <j.h.gerritsen@movage.nl>
2015-06-16 10:40:41 +0200

    Composer update

--- app/config/app.php
+++ app/config/app.php
@@ -113,7 +113,6 @@ return array(
 		'Illuminate\Validation\ValidationServiceProvider',
 		'Illuminate\View\ViewServiceProvider',
 		'Illuminate\Workbench\WorkbenchServiceProvider',
- 		'Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider'
 
 	),
 
