7229f312ca37d38640c5322df5fd899e290809a3
Jan-Henk Gerritsen <j.h.gerritsen@movage.nl>
2015-07-06 16:01:02 +0200

    Add test assertion

--- tests/functional/FormErrorsCept.php
+++ tests/functional/FormErrorsCept.php
@@ -6,6 +6,7 @@ $I->amOnPage('/auth/register');
 $I->click('button[type=submit]');
 
 $I->seeCurrentUrlEquals('/auth/register');
+$I->see('The name field is required.');
 $I->seeFormHasErrors();
 $I->seeFormErrorMessage('name', 'The name field is required.');
 $I->seeFormErrorMessages(array(
