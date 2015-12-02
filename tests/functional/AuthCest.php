dfc946562024010665e3b650627a85f55bc14e2d
Jan-Henk Gerritsen <j.h.gerritsen@movage.nl>
2015-12-02 14:32:08 +0100

    Update tests for refactoring of module

--- tests/functional/AuthCest.php
+++ tests/functional/AuthCest.php
@@ -7,7 +7,7 @@ class AuthCest
 
     private $userAttributes;
 
-    public function  __construct()
+    public function  _before()
     {
         $this->userAttributes= [
             'email' =>  'john@doe.com',
@@ -19,45 +19,41 @@ class AuthCest
 
     public function loginUsingUserRecord(FunctionalTester $I)
     {
-        $I->dontSeeAuthentication();
-
-        $user = User::create($this->userAttributes);
-        $I->amLoggedAs($user);
+        $I->amLoggedAs(User::create($this->userAttributes));
 
         $I->amOnPage(PostsPage::$url);
+
         $I->seeCurrentUrlEquals(PostsPage::$url);
         $I->seeAuthentication();
-
-        $I->logout();
-        $I->dontSeeAuthentication();
     }
 
     public function loginUsingCredentials(FunctionalTester $I)
     {
-        $I->dontSeeAuthentication();
         $I->haveRecord('users', $this->userAttributes);
         $I->amLoggedAs(['email' => 'john@doe.com', 'password' => 'password']);
 
         $I->amOnPage(PostsPage::$url);
+
         $I->seeCurrentUrlEquals(PostsPage::$url);
         $I->seeAuthentication();
-
-        $I->logout();
-        $I->dontSeeAuthentication();
     }
 
-    public function requireAuthenticationForRoute(FunctionalTester $I)
+    public function secureRouteWithoutAuthenticatedUser(FunctionalTester $I)
     {
         $I->haveEnabledFilters();
 
         $I->amOnPage('/secure');
-        $I->seeCurrentUrlEquals('/auth/login');
-        $I->see('Login');
 
-        $I->amLoggedAs(User::create($this->userAttributes));
-        $I->amOnPage('/secure');
-        $I->seeResponseCodeIs(200);
-        $I->see('Hello World');
+        $I->seeCurrentUrlEquals('/auth/login');
     }
 
+    public function secureRouteWithAuthenticatedUser(FunctionalTester $I)
+    {
+        $I->haveEnabledFilters();
+        $I->amLoggedAs(User::create($this->userAttributes));
+
+        $I->amOnPage('/secure');
+
+        $I->see('Hello World');
+    }
 }
